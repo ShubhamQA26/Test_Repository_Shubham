a1257f36db761f309cb24d659c5a6ad6529f497e
ShubhamQA26 <shubhamkarale002@gmail.com>
2024-06-09 18:48:15 +0530

    spell correct

--- src/Java_Test_Program/TestClass1.java
+++ src/Java_Test_Program/TestClass1.java
@@ -3,7 +3,7 @@ package Java_Test_Program;
 public class TestClass1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test the eclipese");
+		System.out.println("Test the eclipse new update");
 
 	}
 
